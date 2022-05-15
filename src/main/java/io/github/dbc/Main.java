83d653959a92034f18bb8179215f47dc63629d16
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-05-15 19:09:27 +0530

    add the solution code for the required problem statement.

--- src/main/java/io/github/dbc/Main.java
+++ src/main/java/io/github/dbc/Main.java
@@ -3,7 +3,7 @@ package io.github.dbc;
 public class Main {
     public static void main(String[] args) {
         DuplicateCharactersCounter counter = new DuplicateCharactersCounter();
-        var duplicateCharacters = counter.countDuplicateCharacters("abcdefghijklmnopqrstuvwxyz");
+        var duplicateCharacters = counter.countDuplicateCharacters("aabcc");
         System.out.println(duplicateCharacters);
     }
 }
